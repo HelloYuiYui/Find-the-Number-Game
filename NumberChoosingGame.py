b6f98c8c9ee27f110be1492f0fb7a577d698b623
Enes <enes35ihsan@gmail.com>
2019-02-08 19:24:56 +0000

    Fixed my old code.

--- NumberChoosingGame.py
+++ NumberChoosingGame.py
@@ -1,4 +1,3 @@
-
 import time
 from random import randint
 import random
@@ -9,13 +8,18 @@ time.sleep(2.0)
 print("I just wanted to play a game")
 time.sleep(1.0)
 
+#defines findTheNumber function
 def findTheNumber():
-    print ("Between which numbers we should choose a number?")
+    print ("Between which numbers I should choose a number?")
+    #creates variables called number 1 and number 2
     num1 = int(input("number 1: "))
     num2 = int(input("number 2: "))
+    #creates a random number between chosen numbers
     game = random.randint(num1, num2)
     print ("choose a number between" + str(num1) + " and " + str(num2))
+    #creates count variable for count the loop
     count = 0
+    #creates a loop until you find the number chosen
     while True:
         count += 1
         answer = int(input("num: "))
@@ -24,8 +28,8 @@ def findTheNumber():
         elif answer > game:
             print ("down")
         else:
-            print ("we have found it!")
-            print ( "you have tried for" + str(count) + "times")
+            print ("you have found it!")
+            print ( "you have tried for " + str(count) + " times")
             time.sleep(10.0)
             break
 
